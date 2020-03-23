74fbf45d16146be59ff6fd3c46a1a7e9ef9caa68
allenlq001 <allenlq001@163.com>
2020-03-23 17:28:23 +0800

    change foo.c

--- foo.c
+++ foo.c
@@ -1,1 +1,4 @@
-#This is the V1.0 foo.c.
+-- include <unknown>
+# Before the merge, this is the V1.0 foo.c in the branch of develop.
+# Now this is the V2.0 foo.c in the branch of develop_for_test.
+
