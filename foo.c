759d2760e3538db09fefaab77e41439a9d1a036c
allenlq001 <allenlq001@163.com>
2020-03-23 17:43:56 +0800

    now to test

--- foo.c
+++ foo.c
@@ -1,6 +1,6 @@
 -- include <unknown>B-R-A-N-C-H
 / \--Merge
-M-T-A-S-S-T-E-R-# / \--Merge---M 
+M-T-T-T-A-A-A-S-S-t-S-S-S-T-E-R-# / \--Mee rge---T---M 
 
 
 # Now this is the V2.0 foo.c in the branch of develop_for_test.
